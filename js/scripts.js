4e72fdfe3e42e1a4f26c3c07c1c5b6b50090aa41
unknown <jv@localhost.com>
2023-03-04 13:06:31 +0100

    Añadida sección ejemplos inicial

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function  ejemplosClick() {
+	document.getElementById("Presentacion").style.display="none";
+	document.getElementById("Ejemplos").style.display="block";
+}
+
+function  presentacionClick() {
+	document.getElementById("Presentacion").style.display="block";
+	document.getElementById("Ejemplos").style.display="none";
+}
