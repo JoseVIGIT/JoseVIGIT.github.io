0de447a201539599fe22be8068a702b9bc81d8f3
unknown <jv@localhost.com>
2023-03-05 12:49:06 +0100

    Cambio en iluminacion de etiquetas

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 function  ejemplosClick() {
 	document.getElementById("Presentacion").style.display="none";
 	document.getElementById("Ejemplos").style.display="block";
-	document.getElementById("btnPresentacion").style.backgroundColor="#fff";
-	document.getElementById("btnEjemplos").style.backgroundColor="#ccc";
+	document.getElementById("btnPresentacion").style.backgroundColor="#ccc";
+	document.getElementById("btnEjemplos").style.backgroundColor="#fff";
 }
 
 function  presentacionClick() {
 	document.getElementById("Presentacion").style.display="block";
 	document.getElementById("Ejemplos").style.display="none";
-	document.getElementById("btnEjemplos").style.backgroundColor="#fff";
-	document.getElementById("btnPresentacion").style.backgroundColor="#ccc";
+	document.getElementById("btnEjemplos").style.backgroundColor="#ccc";
+	document.getElementById("btnPresentacion").style.backgroundColor="#fff";
 }
