7e6b8131a5d14785f9940a8c213dfb312bc3c7da
unknown <jv@localhost.com>
2023-03-05 17:14:57 +0100

    Refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,31 @@
+var inicioToqueX;
 
-	
-	var inicioToqueX;
+function desplazaIni() {
+	inicioToqueX = (event.changedTouches[0].clientX);
+}
+function desplazaFin() {
+	var fin = inicioToqueX - event.changedTouches[0].clientX
+	if (fin > 150) // hacia la izquierda
+		ejemplosClick();
+	if (fin < -150) // hacia la derecha
+		presentacionClick ();
+}
 
-	function desplaza() {
-		inicioToqueX = (event.changedTouches[0].clientX);
-	}
-	function desplaza2() {
-		var fin = inicioToqueX - event.changedTouches[0].clientX
-		if (fin > 150) // hacia la izquierda
-			ejemplosClick();
-		if (fin < -150) // hacia la derecha
-			presentacionClick ();
-	}
+function ejemplosClick() {
+	document.getElementById("Presentacion").style.display="none";
+	document.getElementById("Ejemplos").style.display="block";
+	document.getElementById("btnPresentacion").style.backgroundColor="#ccc";
+	document.getElementById("btnEjemplos").style.backgroundColor="#fff";
+}
 
-	function ejemplosClick() {
-		document.getElementById("Presentacion").style.display="none";
-		document.getElementById("Ejemplos").style.display="block";
-		document.getElementById("btnPresentacion").style.backgroundColor="#ccc";
-		document.getElementById("btnEjemplos").style.backgroundColor="#fff";
-	}
-	
-	function presentacionClick() {
-		document.getElementById("Presentacion").style.display="block";
-		document.getElementById("Ejemplos").style.display="none";
-		document.getElementById("btnEjemplos").style.backgroundColor="#ccc";
-		document.getElementById("btnPresentacion").style.backgroundColor="#fff";
-	}
-	
-	document.getElementById("btnPresentacion").addEventListener("click", presentacionClick);
-	document.getElementById("btnEjemplos").addEventListener("click", ejemplosClick);
+function presentacionClick() {
+	document.getElementById("Presentacion").style.display="block";
+	document.getElementById("Ejemplos").style.display="none";
+	document.getElementById("btnEjemplos").style.backgroundColor="#ccc";
+	document.getElementById("btnPresentacion").style.backgroundColor="#fff";
+}
 
-	document.getElementById("contenido").addEventListener("touchstart", desplaza);
-	document.getElementById("contenido").addEventListener("touchend", desplaza2);
+document.getElementById("btnPresentacion").addEventListener("click", presentacionClick);
+document.getElementById("btnEjemplos").addEventListener("click", ejemplosClick);
+document.getElementById("contenido").addEventListener("touchstart", desplazaIni);
+document.getElementById("contenido").addEventListener("touchend", desplazaFin);
